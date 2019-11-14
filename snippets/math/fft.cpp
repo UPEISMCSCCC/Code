eb0fff4ea30a971070c62b6c90c553f1f8ceccff
JarateKing <scmacintosh@upei.ca>
2019-11-14 13:36:17 -0400

    added fast fourier transform

---
+++ snippets/math/fft.cpp
@@ -0,0 +1,64 @@
+#include <complex>
+#include <vector>
+
+#define ull unsigned long long
+#define cd complex<double>
+
+const double PI = acos(-1);
+int reverse(int num, int lg_n) {
+    int res = 0;
+    for (int i = 0; i < lg_n; i++) {
+        if (num & (1 << i))
+            res |= 1 << (lg_n - 1 - i);
+    }
+    return res;
+}
+void fft(vector<cd>& a, bool invert) {
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert)
+        for (auto x : a)
+            x /= n;
+}
+
+vector<int> fftmult(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1 << (32 - __builtin_clz(a.size() + b.size() - 1));
+    fa.resize(n);
+    fb.resize(n);
+    
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+    
+    vector<int> toret(n);
+    for (int i = 0; i < n; i++)
+        toret[i] = round(fa[i].real() / n);
+    return toret;
+}
