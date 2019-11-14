4ebd01f2ba62e96ce845ad602aa0ae6c4287d4d0
JarateKing <scmacintosh@upei.ca>
2019-11-14 18:12:06 -0400

    added comments to rope

--- snippets/data_structures/rope.cpp
+++ snippets/data_structures/rope.cpp
@@ -4,12 +4,17 @@ using namespace __gnu_cxx;
 // O(log n) insert, delete, concatenate
 
 int main() {
+	// generate rope
 	rope<int> v;
 	for (int i = 0; i < 100; i++)
 		v.push_back(i);
+	
+	// move range to front
 	rope<int> copy = v.substr(10, 10);
 	v.erase(10, 10);
 	v.insert(copy.mutable_begin(), copy);
+	
+	// print elements of rope
 	for (auto it : v)
 		cout << it << " ";
 }
