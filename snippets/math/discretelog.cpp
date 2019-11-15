d2140a243da49f7da16941b75d38381e86fb260a
JarateKing <scmacintosh@upei.ca>
2019-11-14 22:21:05 -0400

    optimized discretelog

--- snippets/math/discretelog.cpp
+++ snippets/math/discretelog.cpp
@@ -3,21 +3,22 @@
 
 #define ll long long
 
+unordered_map<int, int> dlogc;
 int discretelog(int a, int b, int m) {
+	dlogc.clear();
 	ll n = sqrt(m)+1, an = 1;
 	for (int i = 0; i < n; i++)
 		an = (an * a) % m;
 	
-	unordered_map<int, int> v;
 	ll c = an;
 	for (int i = 1; i <= n; i++) {
-		if (!v.count(c)) v[c] = i;
+		if (!dlogc.count(c)) dlogc[c] = i;
 		c = (c * an) % m;
 	}
 	
 	c = b;
 	for (int i = 0; i <= n; i++) {
-		if (v.count(c)) return (v[c] * n - i + m - 1) % (m-1);
+		if (dlogc.count(c)) return (dlogc[c] * n - i + m - 1) % (m-1);
 		c = (c * a) % m;
 	}
 	return -1;
