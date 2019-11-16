6402a889c1bae56b4cc255d98e665ff1991f35a6
JarateKing <scmacintosh@upei.ca>
2019-11-16 13:18:17 -0400

    simplified egcd

--- snippets/math/gcd.cpp
+++ snippets/math/gcd.cpp
@@ -1,13 +1,10 @@
+#include <algorithm>
 #define ll long long
 
 ll egcd(ll a, ll b, ll& x, ll& y) {
-	if (b == 0) {
-		x = 1;
-		y = 0;
-		return a;
-	}
+	if (b == 0) { x = 1; y = 0; return a; }
 	ll gcd = egcd(b, a % b, x, y);
 	x -= a / b * y;
-	ll swap = y; y = x; x = swap;
+	swap(x, y);
 	return gcd;
 }
