337c8d46b0826710ad18455f6ac608185595669d
JarateKing <scmacintosh@upei.ca>
2019-11-19 00:01:50 -0400

    added header code

---
+++ snippets/header.cpp
@@ -0,0 +1,20 @@
+// use better compiler options
+#pragma GCC optimize("Ofast","unroll-loops")
+#pragma GCC target("avx2,fma")
+
+// include everything
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/tree_policy.hpp>
+
+// namespaces
+using namespace std;
+using namespace __gnu_cxx; // rope
+using namespace __gnu_pbds; // tree/trie
+
+// common defines
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
+#define ll long long
+#define ull unsigned ll
+#define i128 __int128
+#define u128 unsigned i128
