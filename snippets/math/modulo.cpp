9f0e0a67fcab5545c320d6b3eb4b7f0f197099b5
JarateKing <scmacintosh@upei.ca>
2019-11-19 00:31:33 -0400

    sped up modulo math

--- snippets/math/modulo.cpp
+++ snippets/math/modulo.cpp
@@ -1,7 +1,7 @@
-#define MOD 1e9+7
-#define madd(a,b,m) (((a%m)+(b%m))%m)
-#define mult(a,b,m) (((a%m)*(b%m))%m)
-#define msub(a,b,m) (((a%m)-(b%m)+m)%m)
+#define MOD 1000000007
+#define madd(a,b,m) (a+b-((a+b-m>=0)?m:0))
+#define mult(a,b,m) ((ull)a*b%m)
+#define msub(a,b,m) (a-b+((a-b<0)?m:0))
 
 ll mpow(ll b, ll e, ll m) {
 	ll x = 1;
