330189b95a721c7d53451037b1c1a9fe4158c7db
JarateKing <scmacintosh@upei.ca>
2019-11-21 15:03:06 -0400

    support int and unsigned int

--- snippets/fastio.cpp
+++ snippets/fastio.cpp
@@ -1,6 +1,14 @@
-// read int fast -- no negatives
-void readn(int& n) {
+void readn(unsigned int& n) {
 	char c; n = 0;
 	while ((c = getchar_unlocked()) != ' ' && c != '\n')
 		n = n * 10 + c - '0';
+}
+
+void readn(int& n) {
+	char c; n = 0; int s = 1;
+	if ((c = getchar_unlocked()) == '-') s = -1;
+	else n = c - '0';
+	while ((c = getchar_unlocked()) != ' ' && c != '\n')
+		n = n * 10 + c - '0';
+	n *= s;
 }
