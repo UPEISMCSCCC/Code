18893f3921135159147842e2208a3f6194809d02
JarateKing <scmacintosh@upei.ca>
2019-12-03 00:54:48 -0400

    converted factorial code into defines

--- snippets/math/factorial.cpp
+++ snippets/math/factorial.cpp
@@ -1,14 +1,8 @@
 // digits in factorial
-int kamenetsky(int n) {
-	return floor((n * log10(n / M_E)) + (log10(2 * M_PI * n) / 2.0)) + 1;
-}
+#define kamenetsky(n) (floor((n * log10(n / M_E)) + (log10(2 * M_PI * n) / 2.0)) + 1)
 
 // approximation of factorial
-int stirlings(int n) {
-	return (n == 1) ? 1 : sqrt(2 * M_PI * n) * pow(n / M_E, n);
-}
+#define stirling(n) ((n == 1) ? 1 : sqrt(2 * M_PI * n) * pow(n / M_E, n))
 
-// log of factorial
-double logfact(int n) {
-	return lgamma(n+1);
-}
+// natural log of factorial
+#define lfactorial(n) (lgamma(n+1))
