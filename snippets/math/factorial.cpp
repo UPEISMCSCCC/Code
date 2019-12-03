c095f202409adc02374c48c19f3bc8129cb6c752
JarateKing <scmacintosh@upei.ca>
2019-12-03 00:50:43 -0400

    added stirling's approximation

--- snippets/math/factorial.cpp
+++ snippets/math/factorial.cpp
@@ -3,6 +3,11 @@ int kamenetsky(int n) {
 	return floor((n * log10(n / M_E)) + (log10(2 * M_PI * n) / 2.0)) + 1;
 }
 
+// approximation of factorial
+int stirlings(int n) {
+	return (n == 1) ? 1 : sqrt(2 * M_PI * n) * pow(n / M_E, n);
+}
+
 // log of factorial
 double logfact(int n) {
 	return lgamma(n+1);
