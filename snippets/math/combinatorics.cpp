33403c0e9cf09f2382b76f858c38f9a733852837
JarateKing <scmacintosh@upei.ca>
2019-12-12 11:36:23 -0400

    added nPr under modulo

--- snippets/math/combinatorics.cpp
+++ snippets/math/combinatorics.cpp
@@ -1,7 +1,14 @@
 ull nPr(ull n, ull r) {
 	ull v = 1;
-	for (int i = n-r + 1; i <= n; i++)
+	for (ull i = n-r+1; i <= n; i++)
 		v *= i;
+	return v;
+}
+
+ull nPr(ull n, ull r, ull m) {
+	ull v = 1;
+	for (ull i = n-r+1; i <= n; i++)
+		v = (v * i) % m;
 	return v;
 }
 
