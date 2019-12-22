746a473f23fac082927c38503b48f26a457b9c46
JarateKing <scmacintosh@upei.ca>
2019-12-22 00:18:36 -0400

    removed unneeded line for lcp

--- snippets/string/lcp.cpp
+++ snippets/string/lcp.cpp
@@ -1,6 +1,5 @@
 string lcp(string* arr, int n) {
 	if (n == 0) return "";
-	if (n == 1) return arr[0];
 	sort(arr, arr + n);
 	string r = ""; int v = 0;
 	while (v < arr[0].length() && arr[0][v] == arr[n-1][v])
