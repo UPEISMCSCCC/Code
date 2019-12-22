e75d792e76e6abb3365f42af02615540b4033f2e
JarateKing <scmacintosh@upei.ca>
2019-12-22 03:39:54 -0400

    reduced fenwick tree size a bit

--- snippets/data_structures/fenwick.cpp
+++ snippets/data_structures/fenwick.cpp
@@ -1,7 +1,6 @@
 // Fenwick tree, array of cumulative sums - O(log n) updates, O(log n) gets
 struct Fenwick {
-	int n;
-	ll* tree;
+	int n; ll* tree;
 	
 	void update(int i, int val) {
 		++i;
@@ -17,15 +16,11 @@ struct Fenwick {
 		for (int i = 1; i <= n; i++)
 			tree[i] = 0;
 	}
-	
 	Fenwick(int* arr, int size) : Fenwick(size) {
 		for (int i = 0; i < n; i++)
 			update(i, arr[i]);
 	}
-	
-	~Fenwick() {
-		delete[] tree;
-	}
+	~Fenwick() { delete[] tree; }
 	
 	ll operator[](int i) {
 		if (i < 0 || i > n) return 0;
@@ -38,7 +33,5 @@ struct Fenwick {
 		return sum;
 	}
 	
-	ll getRange(int a, int b) {
-		return operator[](b) - operator[](a-1);
-	}
+	ll getRange(int a, int b) { return operator[](b) - operator[](a-1); }
 };
