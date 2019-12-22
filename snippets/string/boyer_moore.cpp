f4cc0b6e9125fc9a5f0b4ee1f00d91e7c0bfd40d
JarateKing <scmacintosh@upei.ca>
2019-12-22 01:34:38 -0400

    made badchar a map instead

--- snippets/string/boyer_moore.cpp
+++ snippets/string/boyer_moore.cpp
@@ -1,18 +1,17 @@
+struct defint { int i = -1; };
 vector<int> boyermoore(string txt, string pat) {
-	vector<int> toret;
+	vector<int> toret; unordered_map<char, defint> badchar;
 	int m = pat.size(), n = txt.size();
-	int badchar[256];
-	for (int i = 0; i < 256; i++) badchar[i] = -1;
-	for (int i = 0; i < m; i++) badchar[pat[i]] = i;
+	for (int i = 0; i < m; i++) badchar[pat[i]].i = i;
 	int s = 0;
 	while (s <= n - m) {
 		int j = m - 1;
 		while (j >= 0 && pat[j] == txt[s + j]) j--;
 		if (j < 0) {
 			toret.push_back(s);
-			s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
+			s += (s + m < n) ? m - badchar[txt[s + m]].i : 1;
 		} else
-			s += max(1, j - badchar[txt[s + j]]);
+			s += max(1, j - badchar[txt[s + j]].i);
 	}
 	return toret;
 }
