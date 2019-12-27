b1d5135fc17998c1bbeade43fd87197f2deeb989
JarateKing <scmacintosh@upei.ca>
2019-12-26 22:12:58 -0400

    made ternary search support min or max

--- snippets/algorithm/ternary_search.cpp
+++ snippets/algorithm/ternary_search.cpp
@@ -1,11 +1,13 @@
+// < for max, > for min
+#define TERNCOMP(a,b) (a < b)
 int ternary_search(int a, int b, int (*f)(int)) {
     while (b-a > 4) {
         int m = (a+b)/2;
-        if ((*f)(m) < (*f)(m+1)) a = m;
+        if (TERNCOMP((*f)(m), (*f)(m+1))) a = m;
         else b = m+1;
     }
     for (int i = a+1; i < b+1; i++)
-        if ((*f)(a) < (*f)(i))
+        if (TERNCOMP((*f)(a), (*f)(i)))
             a = i;
     return a;
 }
