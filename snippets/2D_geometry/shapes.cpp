2bbe7bcd3ff4b6efb11007371ba4bc732c3a9449
JarateKing <scmacintosh@upei.ca>
2019-12-28 12:22:18 -0400

    added rectangle intersection

--- snippets/2D_geometry/shapes.cpp
+++ snippets/2D_geometry/shapes.cpp
@@ -5,6 +5,7 @@ double cross(point a, point b) { return imag(conj(a)*b); }
 struct line { point a, b; };
 struct circle { point c; double r; };
 struct triangle { point a, b, c; };
+struct rectangle { point tl, br; };
 
 #define sq(a) ((a)*(a))
 double circumference(circle a) { return 2 * a.r * M_PI; }
@@ -17,4 +18,10 @@ double intersection(circle a, circle b) {
 	double alpha = acos((sq(a.r) + sq(d) - sq(b.r)) / (2 * a.r * d));
 	double beta = acos((sq(b.r) + sq(d) - sq(a.r)) / (2 * b.r * d));
 	return sq(a.r) * (alpha - 0.5 * sin(2 * alpha)) + sq(b.r) * (beta - 0.5 * sin(2 * beta));
+}
+
+double intersection(rectangle a, rectangle b) {
+	double x1 = max(real(a.tl), real(b.tl)), y1 = max(imag(a.tl), imag(b.tl));
+	double x2 = min(real(a.br), real(b.br)), y2 = min(imag(a.br), imag(b.br));
+	return (x2 <= x1 || y2 <= y1) ? 0 : (x2-x1)*(y2-y1);
 }
