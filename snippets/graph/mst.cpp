cc9b2dd15a8962bf8149a64d74a18071b299a7b7
wtaylor17 <wtaylor@upei.ca>
2020-01-07 12:37:41 -0400

    uses vector<vector<edge>>, bitset, changed pq template

--- snippets/graph/mst.cpp
+++ snippets/graph/mst.cpp
@@ -1,39 +1,34 @@
-struct edge{
-	int u,v,w;
-	edge (int _, int __, int ___) { u = _, v = __, w = ___;}
+struct edge {
+    int u,v,w;
+    edge (int u, int v, int w) : u(u), v(v), w(w) {}
+    edge () : u(0), v(0), w(0) {}
 };
+bool operator < (const edge &e1, const edge &e2) { return e1.w < e2.w; }
+bool operator > (const edge &e1, const edge &e2) { return e1.w > e2.w; }
 
-// returns ptr to vector of edges in the mst, reasonably fast
+// returns vector of edges in the mst
 // graph[i] = vector of edges incident to vertex i
 // places total weight of the mst in &total
-vector<edge> *mst(vector<edge> *graph, size_t n, size_t m, long long &total) {
+// if returned vector has size != n-1, there is no MST
+vector<edge> mst(vector<vector<edge>> graph, ll &total) {
     total = 0;
-    const auto compEdge = [](edge e, edge f) { return e.w > f.w; };
-    priority_queue<edge,vector<edge>,decltype(compEdge)> pq(compEdge);
-    vector<edge> *MST = new vector<edge>;
-    bool marked[n];
-    memset(marked,false,n);
-	  marked[0] = true; // mark vertex 0 as visited
-    for (int i=0;i<graph[0].size();i++) pq.push(graph[0][i]); // push all edges incident to vertex 0
-    edge e(0,0,0);
+    priority_queue<edge, vector<edge>, greater<edge>> pq;
+    vector<edge> MST;
+    bitset<20001> marked; // change size as needed
+    marked[0] = 1;
+    for (edge ep : graph[0]) pq.push(ep);
+    edge e;
     int u,v,w,chosen;
-    while(MST->size()!=n-1 && pq.size()!=0) {
-        e = pq.top();
-        pq.pop();
+    while(MST.size()!=graph.size()-1 && pq.size()!=0) {
+        e = pq.top(); pq.pop();
         u = e.u, v = e.v, w = e.w;
         if(marked[u] && marked[v]) continue;
         else if(marked[u]) chosen=v;
         else chosen=u;
-        for(int i=0;i<graph[chosen].size();i++) pq.push(graph[chosen][i]);
-        marked[chosen] = true;
-        MST->push_back(e);
+        for(edge ep : graph[chosen]) pq.push(ep);
+        marked[chosen] = 1;
+        MST.push_back(e);
         total += e.w;
     }
-    if(MST->size()==n-1) {
-        return MST;
-    }
-    else {
-        delete MST;
-        return nullptr;
-    }
+    return MST;
 }
