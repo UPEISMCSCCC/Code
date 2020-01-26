8bc3ed80746eea24114931c5d4d99d1f86ee3b60
JarateKing <scmacintosh@upei.ca>
2020-01-26 16:11:50 -0400

    added contiguous subarray (min / max)

---
+++ snippets/algorithm/minmax_subarray.cpp
@@ -0,0 +1,12 @@
+// max subarray - compare = a < b, reset = a < 0
+// min subarray - compare = a > b, reset = a > 0
+// returns {sum, {start, end}}
+pair<int, pair<int, int>> ContiguousSubarray(int* a, int size, bool(*compare)(int, int), bool(*reset)(int), int defbest = 0) {
+    int best = defbest, cur = 0, start = 0, end = 0, s = 0;
+    for (int i = 0; i < size; i++) {
+        cur += a[i];
+        if ((*compare)(best, cur)) { best = cur; start = s; end = i; }
+        if ((*reset)(cur)) { cur = 0; s = i + 1; }
+    }
+	return {best, {start, end}};
+}
