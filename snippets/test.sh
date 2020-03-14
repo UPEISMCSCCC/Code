116717c1db97c1264fed187f0e72697a768813dc
JarateKing <scmacintosh@upei.ca>
2020-03-14 11:07:12 -0300

    added autotest script

---
+++ snippets/test.sh
@@ -0,0 +1,8 @@
+# compile and test all *.in and *.ans
+g++ -g -O2 -std=gnu++17 -static prog.cpp
+for i in *.in; do
+	f=${i%.in}
+	./a.exe < $i > "$f.out"
+	diff -b -q "$f.ans" "$f.out"
+done
+echo "done"
