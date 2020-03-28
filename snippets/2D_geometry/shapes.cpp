ac896d2a1c6154aa6755e482f9daadbacee56c99
JarateKing <scmacintosh@upei.ca>
2020-03-28 19:05:35 -0300

    added more circle intersections

--- snippets/2D_geometry/shapes.cpp
+++ snippets/2D_geometry/shapes.cpp
@@ -45,6 +45,29 @@ double intersection(circle a, circle b) {
 	double beta = acos((sq(b.r) + sq(d) - sq(a.r)) / (2 * b.r * d));
 	return sq(a.r) * (alpha - 0.5 * sin(2 * alpha)) + sq(b.r) * (beta - 0.5 * sin(2 * beta));
 }
+// -1 outside, 0 inside, 1 tangent, 2 intersection
+int intersection(circle a, circle b, vector<point>& inter) {
+	double d2 = norm(b.c - a.c), rS = a.r + b.r, rD = a.r - b.r;
+	if (d2 > rS * rS) return -1;
+	if (d2 < rD * rD) return 0;
+	double ca = 0.5 * (1 + rS * rD / d2);
+	point z = point(ca, sqrt(a.r * a.r / d2 - ca * ca));
+	inter.push_back(a.c + (b.c - a.c) * z);
+	if (abs(imag(z)) > EPS) inter.push_back(a.c + (b.c - a.c) * conj(z));
+	return inter.size();
+}
+vector<point> intersection(line a, circle c) {
+	vector<point> inter;
+	c.c -= a;
+	b -= a;
+	point m = b * real(c.c / b);
+	double d2 = norm(m - c.c);
+	if (d2 > c.r * c.r) return 0;
+	double l = sqrt((c.r * c.r - d2) / norm(b));
+	inter.push_back(a + m + l * b);
+	if (abs(l) > EPS) inter.push_back(a + m - l * b);
+	return inter;
+}
 
 double intersection(rectangle a, rectangle b) {
 	double x1 = max(real(a.tl), real(b.tl)), y1 = max(imag(a.tl), imag(b.tl));
