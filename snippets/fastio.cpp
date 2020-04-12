bf1ccfeeced5009fbdeab1804787d5f646b9e382
JarateKing <scmacintosh@upei.ca>
2020-04-12 13:37:22 -0300

    improved fastio

--- snippets/fastio.cpp
+++ snippets/fastio.cpp
@@ -1,47 +1,51 @@
-void readn(unsigned int& n) {
+// _unlocked is faster, but not universally supported (windows, codeforces)
+#define inchar() getchar/*_unlocked*/()
+#define outchar(x) putchar/*_unlocked*/(x)
+
+void read(unsigned int& n) {
 	char c; n = 0;
-	while ((c=getchar_unlocked())!=' '&&c!='\n')
+	while ((c=inchar())!=' '&&c!='\n')
 		n = n * 10 + c - '0';
 }
 
-void readn(int& n) {
+void read(int& n) {
 	char c; n = 0; int s = 1;
-	if ((c=getchar_unlocked())=='-') s = -1;
+	if ((c=inchar())=='-') s = -1;
 	else n = c - '0';
-	while ((c=getchar_unlocked())!=' '&&c!='\n')
+	while ((c=inchar())!=' '&&c!='\n')
 		n = n * 10 + c - '0';
 	n *= s;
 }
 
-void readn(ld& n) {
+void read(ld& n) {
 	char c; n = 0;
 	ld m = 0, o = 1; bool d = false; int s = 1;
-	if ((c=getchar_unlocked())=='-') s = -1;
+	if ((c=inchar())=='-') s = -1;
 	else if (c == '.') d = true;
 	else n = c - '0';
-	while ((c=getchar_unlocked())!=' '&&c!='\n') {
+	while ((c=inchar())!=' '&&c!='\n') {
 		if (c == '.') d = true;
 		else if (d) { m=m*10+c-'0'; o*=0.1; }
 		else n = n * 10 + c - '0';
 	}
 	n = s * (n + m * o);
 }
-void readn(double& n) {
-	ld m; readn(m); n = m;
+void read(double& n) {
+	ld m; read(m); n = m;
 }
-void readn(float& n) {
-	ld m; readn(m); n = m;
+void read(float& n) {
+	ld m; read(m); n = m;
 }
 
-void readn(string& s) {
+void read(string& s) {
 	char c; s = "";
-	while((c=getchar_unlocked())!=' '&&c!='\n')
+	while((c=inchar())!=' '&&c!='\n')
 		s += c;
 }
 
 bool readline(string& s) {
 	char c; s = "";
-	while(c=getchar_unlocked()) {
+	while(c=inchar()) {
 		if (c == '\n') return true;
 		if (c == EOF) return false;
 		s += c;
@@ -49,12 +53,12 @@ bool readline(string& s) {
 	return false;
 }
 
-void printn(unsigned int n) {
-	if (n / 10) printn(n / 10);
-	putchar_unlocked(n % 10 + '0');
+void print(unsigned int n) {
+	if (n / 10) print(n / 10);
+	outchar(n % 10 + '0');
 }
 
-void printn(int n) {
-	if (n < 0) { putchar_unlocked('-'); n*=-1; }
-	printn((unsigned int)n);
+void print(int n) {
+	if (n < 0) { outchar('-'); n*=-1; }
+	print((unsigned int)n);
 }
