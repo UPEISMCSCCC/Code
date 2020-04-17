5e2d634877f207ce7f89cc680609be700075dd7d
JarateKing <scmacintosh@upei.ca>
2020-04-17 00:03:04 -0300

    added n-dimensional vector struct

---
+++ snippets/structs.cpp
@@ -0,0 +1,12 @@
+// n-dimension vectors
+// Vec<2, int> v(n, m) = arr[n][m]
+// Vec<2, int> v(n, m, -1) default init -1
+template<int D, typename T>
+struct Vec : public vector<Vec<D-1, T>> {
+  template<typename... Args>
+  Vec(int n=0, Args... args) : vector<Vec<D-1, T>>(n, Vec<D-1, T>(args...)) {}
+};
+template<typename T>
+struct Vec<1, T> : public vector<T> {
+  Vec(int n=0, T val=T()) : vector<T>(n, val) {}
+};
