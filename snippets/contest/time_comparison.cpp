327b77b1a32a19b429d7b248b5ffee7ea1a950a3
JarateKing <scmacintosh@upei.ca>
2020-04-22 00:56:01 -0300

    added contest time comparison snippet

---
+++ snippets/contest/time_comparison.cpp
@@ -0,0 +1,9 @@
+// kattis: 0.50s
+// codeforces: 0.421s
+#include <bits/stdc++.h>
+using namespace std;
+int v = 1e9/2, p = 1;
+int main() {
+    for (int i = 1; i <= v; i++) p *= i;
+    cout << p;
+}
