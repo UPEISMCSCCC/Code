93db077b52509a6629492e25d5bd7e68dd79e9f5
JarateKing <scmacintosh@upei.ca>
2020-05-29 15:02:21 -0300

    made lcp include check for sorted arrays

--- snippets/string/lcp.cpp
+++ snippets/string/lcp.cpp
@@ -1,6 +1,7 @@
-string lcp(string* arr, int n) {
+// longest common prefix of strings in array
+string lcp(string* arr, int n, bool sorted = false) {
 	if (n == 0) return "";
-	sort(arr, arr + n);
+	if (!sorted) sort(arr, arr + n);
 	string r = ""; int v = 0;
 	while (v < arr[0].length() && arr[0][v] == arr[n-1][v])
 		r += arr[0][v++];
