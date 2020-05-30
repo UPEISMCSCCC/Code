c3c3e571047c6b73910a14883b760c71fc0774fc
JarateKing <scmacintosh@upei.ca>
2020-05-30 16:20:48 -0300

    modified ordered set to support template options

--- snippets/data_structures/ordered_set.cpp
+++ snippets/data_structures/ordered_set.cpp
@@ -1,13 +1,14 @@
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+template <typename T>
+using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
 int main()
 {
-	ordered_set o_set; 
+	oset<int> o_set;
 	o_set.insert(5); o_set.insert(1); o_set.insert(3); 
   
 	// get second smallest element
-	cout << *(o_set.find_by_order(1)) << '\n';
+	cout << *(o_set.find_by_order(1));
   
 	// number of elements less than k=4 
-	cout << o_set.order_of_key(4) << '\n';
+	cout << ' ' << o_set.order_of_key(4) << '\n';
 }
