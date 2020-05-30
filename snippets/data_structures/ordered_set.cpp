6ec5f5d79488553f71d84ac2cf13158b0fba432d
JarateKing <scmacintosh@upei.ca>
2020-05-30 16:21:02 -0300

    added ordered map

--- snippets/data_structures/ordered_set.cpp
+++ snippets/data_structures/ordered_set.cpp
@@ -1,5 +1,7 @@
 template <typename T>
 using oset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template <typename T, typename D>
+using omap = tree<T,D,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
 int main()
 {
@@ -11,4 +13,10 @@ int main()
   
 	// number of elements less than k=4 
 	cout << ' ' << o_set.order_of_key(4) << '\n';
+	
+	// equivalent with ordered map
+	omap<int,int> o_map;
+	o_map[5]=1;o_map[1]=2;o_map[3]=3;
+	cout << (*(o_map.find_by_order(1))).first;
+	cout << ' ' << o_map.order_of_key(4) << '\n';
 }
