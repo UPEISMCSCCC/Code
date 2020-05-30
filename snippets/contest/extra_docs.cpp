daa2d146f7d0d6529ec2a3326c0515c74dcfd888
JarateKing <scmacintosh@upei.ca>
2020-05-30 13:41:22 -0300

    added extra docs for gcc implementations

---
+++ snippets/contest/extra_docs.cpp
@@ -0,0 +1,18 @@
+// log2 floor
+__lg(n)
+
+// number of 1 bits
+// can add ll like popcountll for long longs
+__builtin_popcount(n)
+
+// number of trailing zeroes
+__builtin_ctz(n)
+
+// number of leading zeroes
+__builtin_clz(n)
+
+// 1-indexed least significant 1 bit
+__builtin_ffs(n)
+
+// parity of number
+__builtin_parity(n)
