b0c00a7434c4de9826b163fa5e325225dc5ebd33
JarateKing <scmacintosh@upei.ca>
2020-05-30 12:53:26 -0300

    added string util trim

--- snippets/string/utilities.cpp
+++ snippets/string/utilities.cpp
@@ -1,6 +1,10 @@
 void lowercase(string& s) {
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
 }
 void uppercase(string& s) {
-    transform(s.begin(), s.end(), s.begin(), ::toupper);
+	transform(s.begin(), s.end(), s.begin(), ::toupper);
+}
+void trim(string &s) {
+	s.erase(s.begin(),find_if_not(s.begin(),s.end(),[](int c){return isspace(c);}));
+	s.erase(find_if_not(s.rbegin(),s.rend(),[](int c){return isspace(c);}).base(),s.end());
 }
