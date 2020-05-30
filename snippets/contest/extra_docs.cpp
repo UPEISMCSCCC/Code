7eb9df31bdff8bca3632c5cd2e9c60c0ccb96acc
JarateKing <scmacintosh@upei.ca>
2020-05-30 13:54:57 -0300

    added types to extra docs

--- snippets/contest/extra_docs.cpp
+++ snippets/contest/extra_docs.cpp
@@ -1,18 +1,26 @@
+// 128-bit integer
+__int128 a;
+unsigned __int128 b;
+
+// 128-bit float
+// minor improvements over long double
+__float128 c;
+
 // log2 floor
-__lg(n)
+__lg(n);
 
 // number of 1 bits
 // can add ll like popcountll for long longs
-__builtin_popcount(n)
+__builtin_popcount(n);
 
 // number of trailing zeroes
-__builtin_ctz(n)
+__builtin_ctz(n);
 
 // number of leading zeroes
-__builtin_clz(n)
+__builtin_clz(n);
 
 // 1-indexed least significant 1 bit
-__builtin_ffs(n)
+__builtin_ffs(n);
 
 // parity of number
-__builtin_parity(n)
+__builtin_parity(n);
