b83619064f38959a021277cb563bd5b23e956360
JarateKing <scmacintosh@upei.ca>
2020-05-30 12:59:10 -0300

    added string util split

--- snippets/string/utilities.cpp
+++ snippets/string/utilities.cpp
@@ -8,3 +8,9 @@ void trim(string &s) {
 	s.erase(s.begin(),find_if_not(s.begin(),s.end(),[](int c){return isspace(c);}));
 	s.erase(find_if_not(s.rbegin(),s.rend(),[](int c){return isspace(c);}).base(),s.end());
 }
+vector<string> split(string& s, char token) {
+    vector<string> v; stringstream ss(s);
+    for (string e;getline(ss,e,token);)
+        v.push_back(e);
+    return v;
+}
