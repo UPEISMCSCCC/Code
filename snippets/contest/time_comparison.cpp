daef805560b89984f106ad020b50bdc67b599498
JarateKing <scmacintosh@upei.ca>
2020-06-01 17:22:08 -0300

    converted space indentation to tabs

--- snippets/contest/time_comparison.cpp
+++ snippets/contest/time_comparison.cpp
@@ -5,6 +5,6 @@
 using namespace std;
 int v = 1e9/2, p = 1;
 int main() {
-    for (int i = 1; i <= v; i++) p *= i;
-    cout << p;
+	for (int i = 1; i <= v; i++) p *= i;
+	cout << p;
 }
--- snippets/math/graycode.cpp
+++ snippets/math/graycode.cpp
@@ -1,9 +1,9 @@
 ull graycode2ull(ull n) { 
-    ull i = 0;
-    for (; n; n = n >> 1) i ^= n; 
-    return i; 
+	ull i = 0;
+	for (; n; n = n >> 1) i ^= n; 
+	return i; 
 }
 
 ull ull2graycode(ull n) {
-    return n ^ (n >> 1);
+	return n ^ (n >> 1);
 }
