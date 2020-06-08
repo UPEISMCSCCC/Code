ffd47b793fc3963a876cba5ff3391330fd2436db
JarateKing <scmacintosh@upei.ca>
2020-06-08 09:16:54 -0300

    added addition and subtraction

--- snippets/math/matrix.cpp
+++ snippets/math/matrix.cpp
@@ -3,4 +3,20 @@ struct Mat : public Vec<2, T> {
     int w, h;
     Mat(int x, int y) : Vec<2, T>(x, y), w(x), h(y) {}
     static Mat<T> identity(int n) { Mat<T> m(n,n); for (int i=0;i<n;i++) m[i][i] = 1; return m; }
+	Mat operator+(const Mat& m) {
+	    Mat<T> toret(w,h);
+		for (int i = 0; i < w; i++)
+			for (int j = 0; j < h; j++)
+				toret[i][j] = (*this)[i][j] + m[i][j];
+		return toret;
+	}
+	Mat operator-(const Mat& m) {
+	    Mat<T> toret(w,h);
+		for (int i = 0; i < w; i++)
+			for (int j = 0; j < h; j++)
+				toret[i][j] = (*this)[i][j] - m[i][j];
+		return toret;
+	}
+	Mat operator+=(const Mat& m) { *this = *this + m; return *this; }
+	Mat operator-=(const Mat& m) { *this = *this - m; return *this; }
 };
