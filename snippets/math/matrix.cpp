41a356bde5b34e4947595e5861750d641605c223
JarateKing <scmacintosh@upei.ca>
2020-06-08 10:33:33 -0300

    added matrix to notebook

--- snippets/math/matrix.cpp
+++ snippets/math/matrix.cpp
@@ -1,8 +1,8 @@
 template<typename T>
 struct Mat : public Vec<2, T> {
-    int w, h;
-    Mat(int x, int y) : Vec<2, T>(x, y), w(x), h(y) {}
-    static Mat<T> identity(int n) { Mat<T> m(n,n); for (int i=0;i<n;i++) m[i][i] = 1; return m; }
+	int w, h;
+	Mat(int x, int y) : Vec<2, T>(x, y), w(x), h(y) {}
+	static Mat<T> identity(int n) { Mat<T> m(n,n); for (int i=0;i<n;i++) m[i][i] = 1; return m; }
 	Mat<T>& operator+=(const Mat<T>& m) {
 		for (int i = 0; i < w; i++)
 			for (int j = 0; j < h; j++)
@@ -16,19 +16,19 @@ struct Mat : public Vec<2, T> {
 		return *this;
 	}
 	Mat<T> operator*(const Mat<T>& m) {
-	    Mat<T> z(w,m.h);
-	    for (int i = 0; i < w; i++)
-	        for (int j = 0; j < h; j++)
-	            for (int k = 0; k < m.h; k++)
-	                z[i][k] += (*this)[i][j] * m[j][k];
+		Mat<T> z(w,m.h);
+		for (int i = 0; i < w; i++)
+			for (int j = 0; j < h; j++)
+				for (int k = 0; k < m.h; k++)
+					z[i][k] += (*this)[i][j] * m[j][k];
 	   return z;
 	}
 	Mat<T> operator+(const Mat<T>& m) { Mat<T> a=*this; return a+=m; }
 	Mat<T> operator-(const Mat<T>& m) { Mat<T> a=*this; return a-=m; }
 	Mat<T>& operator*=(const Mat<T>& m) { return *this = (*this)*m; }
 	Mat<T> power(int n) {
-	    Mat<T> a = Mat<T>::identity(w),m=*this;
-	    for (;n;n/=2,m*=m) if (n&1) a *= m;
-	    return a;
+		Mat<T> a = Mat<T>::identity(w),m=*this;
+		for (;n;n/=2,m*=m) if (n&1) a *= m;
+		return a;
 	}
 };
