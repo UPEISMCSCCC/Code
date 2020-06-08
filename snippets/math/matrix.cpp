7c3c2e10fbe7e725810f2e149c5e26aac8988f43
JarateKing <scmacintosh@upei.ca>
2020-06-08 08:58:24 -0300

    added basic methods to matrix

--- snippets/math/matrix.cpp
+++ snippets/math/matrix.cpp
@@ -1,4 +1,6 @@
 template<typename T>
 struct Mat : public Vec<2, T> {
-    Mat(int x, int y) : Vec<2, T>(x, y) {}
+    int w, h;
+    Mat(int x, int y) : Vec<2, T>(x, y), w(x), h(y) {}
+    static Mat<T> identity(int n) { Mat<T> m(n,n); for (int i=0;i<n;i++) m[i][i] = 1; return m; }
 };
