a0d457456185c16a2059e7bb72a172488b3667a0
JarateKing <scmacintosh@upei.ca>
2020-06-10 11:48:45 -0300

    added barebones test setup

---
+++ tests/test_all.sh
@@ -0,0 +1,2 @@
+g++ -g -O2 -std=gnu++17 -static math/digitcount.cpp
+./a.exe
---
+++ tests/test_header.h
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned ll
+#define ld long double
