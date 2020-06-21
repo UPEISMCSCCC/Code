4dcd1afe2246c197c418fbf0bebbe8c4483fe07b
JarateKing <scmacintosh@upei.ca>
2020-06-21 00:24:36 -0300

    added tests for string splitting

--- tests/string/utilities.cpp
+++ tests/string/utilities.cpp
@@ -3,6 +3,7 @@
 
 int main() {
 	string s;
+	vector<string> v;
 
 	s = "AbcDefGhi";
 	lowercase(s);
@@ -24,4 +25,11 @@ int main() {
 	s = "   abc   ";
 	trim(s);
 	assert (s == "abc");
+	
+	s = "a,b,c";
+	v = split(s, ',');
+	assert(v.size() == 3 && v[0] == "a" && v[1] == "b" && v[2] == "c");
+	s = "abc";
+	v = split(s, ',');
+	assert(v.size() == 1 && v[0] == "abc");
 }
