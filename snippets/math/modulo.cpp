964a234248c0368773634352777ce0c851712134
JarateKing <scmacintosh@upei.ca>
2020-07-12 12:07:42 -0300

    added tonelli shanks

--- snippets/math/modulo.cpp
+++ snippets/math/modulo.cpp
@@ -35,3 +35,25 @@ ll mdiv_compmod(int a, int b, int m) {
 ll mdiv_primemod (int a, int b, int m) {
 	return mult(a, mpow(b, m-2, m), m);
 }
+
+// tonelli shanks = sqrt(n) % m, m is prime
+ll legendre(ll a, ll m){
+	if (a % m==0) return 0;
+	if (m == 2) return 1;
+	return mpow(a,(m-1)/2,m);
+}
+ll msqrt(ll n, ll m) {
+	ll s = __builtin_ctzll(m-1), q = (m-1ll)>>s, z = rand()%(m-1)+1;
+	if (m == 2) return 1;
+	if (s == 1) return mpow(n,(m+1)/4ll,m);
+	while (legendre(z,m)!=m-1) z = rand()%(m-1)+1;
+	ll c = mpow(z,q,m), r = mpow(n,(q+1)/2,m), t = mpow(n,q,m), M = s;
+	while (t != 1){
+		ll i=1, ts = (t * t) % m;
+		while (ts != 1) i++, ts = (ts * ts) % m;
+		ll b = c;
+		for (int j = 0; j < M-i-1; j++) b = (b * b) % m;
+		r = r * b % m; c = b * b % m; t = t * c % m; M = i;
+	}
+	return r;
+}
