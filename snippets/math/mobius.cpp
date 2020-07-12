0878a86bdddb5f2f7cdcc089bd2bd0996e9f9993
JarateKing <scmacintosh@upei.ca>
2020-07-12 11:18:46 -0300

    added mobius function sieve to notebook

--- snippets/math/mobius.cpp
+++ snippets/math/mobius.cpp
@@ -1,6 +1,6 @@
 const int MAXN = 10000000;
 // mu[n] = 0 iff n has no square factors
-// 1 if even number of prime factors, -1 for odd
+// 1 = even number prime factors, -1 = odd
 short mu[MAXN] = {0,1};
 void mobius(){
 	for (int i = 1; i < MAXN; i++)
