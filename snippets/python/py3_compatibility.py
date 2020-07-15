4dc70bdd2c3a4a3b26ef368aa47430699f02d71d
JarateKing <scmacintosh@upei.ca>
2020-07-15 12:46:30 -0300

    added python 3 compatibility to notebook

--- snippets/python/py3_compatibility.py
+++ snippets/python/py3_compatibility.py
@@ -1,5 +1,5 @@
 import sys
 from __future__ import division, print_function
 if sys.version_info[0] < 3:
-    from __builtin__ import xrange as range
-    from future_builtins import ascii, filter, hex, map, oct, zip
+	from __builtin__ import xrange as range
+	from future_builtins import ascii, filter, hex, map, oct, zip
