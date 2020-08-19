2805855b24cccc04161bd4f45b3011aacc839b24
JarateKing <scmacintosh@upei.ca>
2020-08-19 17:41:22 -0300

    added fastmod

---
+++ snippets/optimization/fastmod.cpp
@@ -0,0 +1,8 @@
+// faster modulo with constant modulus
+struct FastMod {
+	ull b, m;
+	FastMod(ull b) : b(b), m(-1ULL / b) {}
+	ull reduce(ull a) { // a % b + (0 or b)
+		return a - (ull)((__uint128_t(m) * a) >> 64) * b;
+	}
+};
