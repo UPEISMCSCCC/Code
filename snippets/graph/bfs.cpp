e908e65ba577b3786573d8466286d5d315a86d87
JarateKing <scmacintosh@upei.ca>
2020-10-14 09:42:56 -0300

    added optimization for bfs

--- snippets/graph/bfs.cpp
+++ snippets/graph/bfs.cpp
@@ -1,13 +1,18 @@
 // adjacency list named 'graph'
+// - if you only need to bfs once
+// visited can be gutted for parent
+//- path reconstruction in reverse
+// flip start/end only in undirected graphs
+// - can store a distance array too
 int visited[MAX];
 int parent[MAX];
 int vc = 0;
 vector<int> bfs(int start, int end) {
-	vc++;
-	visited[start] = vc;
+	visited[start] = ++vc;
 	parent[start] = -1;
 	queue<int> q;
 	q.push(start);
+	int f = 1, m = graph.size();
 	while (!q.empty()) {
 		int v = q.front(); q.pop();
 		for (auto e : graph[v]) {
@@ -15,7 +20,7 @@ vector<int> bfs(int start, int end) {
 				visited[e] = vc;
 				q.push(e);
 				parent[e] = v;
-				if (e == end) goto DONE;
+				if (++f == m || e == end) goto DONE;
 			}
 		}
 	}
