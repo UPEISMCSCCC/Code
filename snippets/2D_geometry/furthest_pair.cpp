0647cceaca21dde3cb2e22895b0ba53396920965
William Taylor-Melanson <wtaylor@upei.ca>
2020-10-26 09:38:19 -0300

    Update furthest_pair.cpp

--- snippets/2D_geometry/furthest_pair.cpp
+++ snippets/2D_geometry/furthest_pair.cpp
@@ -1,5 +1,6 @@
 // returns diameter (max pairwise distance) of points on a convex hull.
 // needs that all points are distinct and none are collinear
+// takes time linear in the size of the hull
 pair<point,point> hull_diameter(convex_polygon poly) {
     vector<point> &S = poly.points;
     if(S.size() < 2) return {point(), point()};
@@ -15,7 +16,7 @@ pair<point,point> hull_diameter(convex_polygon poly) {
 }
 
 // wraps the hull_diameter, so it works for any set of points
-// relies on the convex_hull code
+// relies on the convex_hull code, so O(nlogn) total
 pair<point,point> farthest_pair(vector<point> &V) {
     return hull_diameter(convexhull(polygon(V), false));
 }
